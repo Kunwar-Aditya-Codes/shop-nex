b19959a56f3b4f7fc669f8311c351c69d802e91b
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-06-14 06:00:15 +0530

    added: database connection

---
+++ server/server/config/connection.ts
@@ -0,0 +1,30 @@
+import { Sequelize } from 'sequelize';
+
+let sequelize: Sequelize;
+if (process.env.NODE_ENV !== 'production') {
+  sequelize = new Sequelize('shopnex', 'postgres', 'aditya', {
+    host: 'localhost',
+    port: 5432,
+    dialect: 'postgres',
+    logging: false,
+    dialectModule: require('pg'),
+  });
+} else {
+  sequelize = new Sequelize(process.env.POSTGRES_URL as string, {
+    dialectModule: require('pg'),
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false,
+      },
+    },
+  });
+}
+
+const startDatabaseConnection = async () => {
+  await sequelize.authenticate();
+  console.log('Database connected');
+};
+
+export { sequelize, startDatabaseConnection };
--- server/server/index.ts
+++ server/server/index.ts
@@ -1,1 +1,25 @@
-import express from "express";
+require('express-async-errors');
+require('dotenv').config();
+import express from 'express';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+
+import { sequelize, startDatabaseConnection } from './config/connection';
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+app.use(cors());
+
+app.get('/', (req, res) => {
+  res.send('Shop Nex Express Server');
+});
+
+startDatabaseConnection().then(async () => {
+  await sequelize.sync({ alter: true });
+  app.listen(process.env.PORT, () => {
+    console.log(`Server running on port ${process.env.PORT}`);
+  });
+});
