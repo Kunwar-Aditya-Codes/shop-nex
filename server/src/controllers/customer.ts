b8662dc3267ca9d542d2b9564c7fc2f042818389
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-06-15 09:23:36 +0530

    feature: update customer

--- server/src/controllers/customer.ts
+++ server/src/controllers/customer.ts
@@ -44,7 +44,36 @@ export const getProfile = async (req: Request, res: Response) => {
  * @returns { success , customer } res
  */
 export const updateProfile = async (req: Request, res: Response) => {
-  
+  const { id } = req;
+
+  if (!id) {
+    return res.status(400).json({
+      success: false,
+      message: 'Invalid request',
+    });
+  }
+
+  const updatedCustomer = await Customer.update(
+    { ...req.body },
+    { where: { userId: id } }
+  );
+
+  if (!updatedCustomer) {
+    return res.status(404).json({
+      success: false,
+      message: 'Customer not found',
+    });
+  }
+
+  const foundCustomer = await Customer.findOne({
+    where: { userId: id },
+    attributes: { exclude: ['password'] },
+  });
+
+  return res.status(200).json({
+    success: true,
+    customer: foundCustomer,
+  });
 };
 
 /**
