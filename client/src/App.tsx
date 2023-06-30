47c4b4aa8c4f19874a0bdee2c19818bb26562bd3
Kunwar Aditya <adityakunwar0147@gmail.com>
2023-06-30 18:22:31 +0530

    added: register comp

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
+import Register from "./pages/Register";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Home />} />
         <Route path="/sign_in" element={<Login />} />
+        <Route path="/sign_up" element={<Register />} />
       </Route>
     </Routes>
   );
--- client/src/pages/Register.tsx
+++ client/src/pages/Register.tsx
@@ -2,17 +2,33 @@ import { Link } from "react-router-dom";
 import { AiOutlineEyeInvisible, AiOutlineEye } from "react-icons/ai";
 import { useState } from "react";
 
-const Login = () => {
+type Props = {};
+const Register = (props: Props) => {
   const [show, setShow] = useState(false);
 
   return (
     <div className="flex flex-grow items-center justify-center">
       <div className="flex w-full max-w-xl flex-col items-center justify-center px-4">
         <form className="flex w-full flex-col space-y-8 rounded-xl  px-4 text-lg md:p-10 ">
+          <div className="flex w-full flex-col space-y-4 lg:flex-row lg:justify-between lg:space-x-2 lg:space-y-0">
+            <input
+              type="text"
+              placeholder="First Name"
+              autoFocus
+              required
+              className="w-full rounded-md border border-zinc-900 bg-transparent px-4 py-3 text-sm tracking-wide outline-none focus:border-white"
+            />
+            <input
+              type="text"
+              placeholder="Last Name"
+              required
+              className="w-full rounded-md border border-zinc-900 bg-transparent px-4 py-3 text-sm tracking-wide outline-none focus:border-white"
+            />
+          </div>
+
           <input
             type="email"
             placeholder="Email"
-            autoFocus
             required
             className="rounded-md border border-zinc-900 bg-transparent px-4 py-3 text-sm tracking-wide outline-none focus:border-white"
           />
@@ -38,18 +54,18 @@ const Login = () => {
             </div>
           </div>
           <button className="rounded-md bg-white/75 py-2 font-light uppercase tracking-wider text-black outline-none disabled:bg-zinc-900 disabled:text-white ">
-            Sign In
+            Sign Up
           </button>
         </form>
 
         <p className="mt-4 px-4 text-sm  tracking-wider text-zinc-400">
-          No account ?
+          Have account ?
           <span className="ml-1 text-white underline underline-offset-4">
-            <Link to="/sign_up">New Account</Link>
+            <Link to="/sign_in">Sign In</Link>
           </span>
         </p>
       </div>
     </div>
   );
 };
-export default Login;
+export default Register;
